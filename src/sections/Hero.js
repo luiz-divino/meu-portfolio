20fb443f79d28089a3d43d005f45f7b8bcfe7774
unknown <nandodivinodev@gmail.com>
2025-10-26 16:41:52 -0300

    alterações de estilo e cores

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,13 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import imgBackground from "../assets/img/background.jpg"
 
 function Hero() {
   return (
-    <section className="hero bg-blue-600 h-screen flex items-center justify-center">
-      <div className="font-sans md:font-serif text-center py-20 text-white">
+    <section className="hero h-screen flex items-center justify-center" style={{ backgroundImage: `url(${imgBackground})`, backgroundSize: 'cover' }}>
+      <div className="font-sans font-bold md:font-serif text-center py-20 text-blue-600">
         <h1 className="text-4xl">LUIZ FERNANDO</h1>
-        <p className="mt-4 text-2xl pb-6">Desenvolvedor Full-stack junior</p>
-      <a href="#about" className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">Sobre mim</a>
+        <p className="mt-4 text-2xl pb-6 font-semibold text-gray-900">Desenvolvedor Full-stack junior</p>
+      <a href="#about" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-900 transition duration-300">Sobre mim</a>
       </div>
 
       
