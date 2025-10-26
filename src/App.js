7b070b8232c36aee494ae6af8a637fb849547e66
unknown <nandodivinodev@gmail.com>
2025-10-26 16:40:43 -0300

    exibição de nova seção

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './sections/Hero';
 import About from './sections/About';
 import Skills from './sections/Skills';
+import ProjectsCard from './sections/ProjectsCard';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <About />
       <Skills />
+      <ProjectsCard />
     </div>
   );
 }
