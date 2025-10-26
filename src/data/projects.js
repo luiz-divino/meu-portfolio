16b55d728bbc5f6876f315b3a07f4b18e3cd97c0
unknown <nandodivinodev@gmail.com>
2025-10-26 16:45:47 -0300

    pasta responsavel por guardar arquivos com dados de projetos e skills

---
+++ src/data/projects.js
@@ -0,0 +1,16 @@
+import React from "react";
+import projetoFutebol from "../assets/img/projetoFutebol.png"
+import adivinhePersonagem from "../assets/img/projetoPersonagens.png"
+
+ const projectsData = [
+    {
+        id:1,
+        image: projetoFutebol,
+    },
+    {
+        id:2,
+        image: adivinhePersonagem,
+    }
+];
+
+export default projectsData;
---
+++ src/data/skillsData.js
@@ -0,0 +1,35 @@
+import html from '../icons/html.svg';
+import css from '../icons/css.svg';
+import javascript from '../icons/javascript.svg';
+import bootstrap from '../icons/bootstrap.svg';
+import react from '../icons/react.svg';
+import tailwind from '../icons/tailwindcss.svg';
+import vercel from '../icons/vercel.svg';
+import github from '../icons/github.svg';
+import git from '../icons/git.svg';
+import node from '../icons/nodejs.svg';
+import postegresql from '../icons/postgresql.svg';
+
+
+ const skillsData  = {
+    "front-end": {
+        html: html,
+        css: css,
+        javascript: javascript,
+        bootstrap: bootstrap,
+        react: react,
+        tailwind: tailwind
+    },
+    "back-end": {
+        node: node,
+        postegresql: postegresql
+    },
+    "deployment & version control": {
+        vercel: vercel,
+        github: github,
+        git: git
+
+    }
+};
+
+export default skillsData;
