d17e0f1a0f7fde20b7ba2ec66af6ee8834112422
unknown <nandodivinodev@gmail.com>
2025-10-26 16:44:06 -0300

    pequena alteração no caminho de importação de arquivos para utilização

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,14 +1,13 @@
 import React from "react";
-import SkillsData from "../components/portfolio/SkillsData";
-
+import skillsData from "../data/skillsData";
 
 function Skills() {
     return (
         <div className="h-screen flex items-center justify-center flex-col space-y-8 flex-wrap bg-gray-300">
-            <h1 className="text-4xl font-bold mb-10">Skills Section</h1>
+            <h1 className="text-4xl font-bold mb-10">My Skills</h1>
 
             <div className="flex space-x-4 flex-wrap justify-center">
-                {Object.entries(SkillsData).map(([category, icons]) => (
+                {Object.entries(skillsData).map(([category, icons]) => (
                     <div key={category} className="flex flex-col items-center gap-3">
 
                         <h2 className="text-2xl font-semibold">{category}</h2>
