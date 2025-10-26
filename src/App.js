dee09efc2cade5fefc9d1c30562e6da2363f98fb
unknown <nandodivinodev@gmail.com>
2025-10-26 01:19:36 -0300

    exibindo as seções

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Hero from './sections/Hero';
+import About from './sections/About';
+import Skills from './sections/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Hero />
+      <About />
+      <Skills />
     </div>
   );
 }
