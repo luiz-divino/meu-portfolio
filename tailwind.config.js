147629a13a7b38cc97c92423b11e9797b59088dd
unknown <nandodivinodev@gmail.com>
2025-10-26 01:17:48 -0300

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", // Se você usa Vite
+    "./src/**/*.{js,ts,jsx,tsx}", // Para seus arquivos React
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
