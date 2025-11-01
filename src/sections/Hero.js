68b20fd58b4878541edee41668a4b22ac1b8f2a8
unknown <nandodivinodev@gmail.com>
2025-11-01 02:02:01 -0300

    personalização de fontes

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -6,12 +6,12 @@ function Hero() {
     <section id="home" className="hero h-screen flex items-center justify-center" style={{ backgroundImage: `url(${imgBackground})`, backgroundSize: 'cover' }}>
       <div className="font-sans font-bold md:font-serif text-center py-20 text-blue-600 ">
 
-        <div className="flex text-center">
+        <div className="flex justify-center">
           <p className="bg-yellow-600 border border-red-50 rounded w-20 text-2xl mb-5 mr-3">Hello</p>
            <p className="text-2xl">I'm</p>
         </div>
 
-        <h1 className="nome text-4xl">LUIZ FERNANDO</h1>
+        <h1 className="nome text-5xl">LUIZ FERNANDO</h1>
 
         <p className="mt-4 text-2xl pb-6 font-bold text-blue-600">Software <span className="text-yellow-600">Engineer</span></p>
 
