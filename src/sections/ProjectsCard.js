8bcb1d7ddc52fdd4e72e99596f0b3e85ec9fb152
unknown <nandodivinodev@gmail.com>
2025-12-11 17:45:13 -0300

    inserindo os dados dos novos projetos para serem exibidos, além de solucionar erro de dimensões dos banners de cada projeto e padronizar os tamanhos

--- src/sections/ProjectsCard.js
+++ src/sections/ProjectsCard.js
@@ -3,14 +3,18 @@ import projectsData from "../data/projects";
 
 function ProjectsCard() {
     return (
-        <section id="projects" className="h-screen flex flex-col items-center justify-center">
-            <h1 className="text-4xl font-bold mb-10">Latest Projects</h1>
-            <div className="flex flex-wrap justify-center gap-8 px-4" >
+        <section id="projects" className="h-full flex flex-col items-center justify-center mb-8">
+            <h1 className="text-4xl font-bold py-10">Latest Projects</h1>
+            <div className="flex flex-wrap justify-center gap-8 px-4 group relative w-full overflow-hidden" >
                 {projectsData.map((project) => (
-                    <div key={project.id} >
-                        <img className="h-auto border border-gray-900 rounded" src={project.image} alt={`Project ${project.id}`} />
+                    <div key={project.id} className="w-80 h-52 relative group overflow-hidden rounded border border-gray-900">
+                        <a href={project.url}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title={`link do projeto ${project.id}`}>
+                            <img className="w-full h-full object-cover" src={project.image} alt={`Project ${project.id}`} />
+                        </a>
                     </div>
-
                 ))}
             </div>
         </section>
