622828cf8fda30d350ce159e330d069958eae44d
unknown <nandodivinodev@gmail.com>
2025-12-11 17:39:04 -0300

    adicão de novos projetos e remoção de projetos com erros

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,20 @@
 import React from "react";
 import projetoFutebol from "../assets/img/projetoFutebol.png"
 import adivinhePersonagem from "../assets/img/projetoPersonagens.png"
+import dataValidator from '../assets/img/dataValidator.jpeg'
+import rh from '../assets/img/RH.jpeg'
 
  const projectsData = [
     {
         id:1,
-        image: projetoFutebol,
-        description: "Projeto desenvolvido em grupo durante o curso de Desenvolvimento Web da Trybe. O objetivo do projeto era desenvolver uma aplicação com back-end em Node.js, Express e Sequelize, e front-end em React e Redux, consumindo uma API RESTful para gerenciar um campeonato de futebol.",
+        image: dataValidator,
+        url: 'https://github.com/luiz-divino/data-validator'
     },
     {
         id:2,
-        image: adivinhePersonagem,
-        description: "Projeto desenvolvido em grupo durante o curso de Desenvolvimento Web da Trybe. O objetivo do projeto era desenvolver um jogo de adivinhação de personagens utilizando React, onde os jogadores tentam adivinhar o personagem secreto com base em pistas fornecidas.",
-    }
+        image: rh,
+        url: 'https://github.com/luiz-divino/RH-system'
+    },
 ];
 
 export default projectsData;
