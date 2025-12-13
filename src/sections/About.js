274d92aca56899f01f4e9f4e9ce44d727695b27e
unknown <nandodivinodev@gmail.com>
2025-12-12 22:29:17 -0300

    ajustes de espaçamento

--- src/sections/About.js
+++ src/sections/About.js
@@ -5,17 +5,17 @@ import Github from "../assets/img/githubProfile.svg";
 function About() {
     return (
         <div id="about">
-            <section className="h-auto flex items-center justify-center flex-col gap-6 px-6 text-justify py-12 bg-gray-100">
+            <section className="h-auto flex items-center justify-center flex-col gap-6 px-6 text-justify py-12"  >
                 <h2 className="text-4xl font-bold text-blue-700 py-2">Sobre <span className="text-yellow-600">Mim</span></h2>
-                <p className="text-lg font-serif">
+                <p className="about-me text-lg">
                     Sou um desenvolvedor full-stack júnior com uma paixão por arquitetar soluções web completas. Atualmente com 20 anos e cursando Engenharia de Software no IBMEC-RJ, eu trago uma mentalidade estruturada para o desenvolvimento, sempre buscando escrever códigos limpos, eficientes e escaláveis.
                     Meu foco é dominar todo o ecossistema web, desde a criação de interfaces de usuário ricas (front-end) até a implementação da lógica de negócios e bancos de dados (back-end). Estou sempre pronto para mergulhar em novos desafios e aprender continuamente.
                 </p>
 
-                <div className='flex items-center justify-center flex-col mt-6'>
+                <div className='flex items-center justify-center flex-col'>
                     <h3 className="text-2xl font-bold text-blue-600">Veja mais sobre mim!</h3>
 
-                    <div className="flex items-center justify-center mt-4">
+                    <div className="flex items-center justify-center ">
                         <a href="https://www.linkedin.com/in/luizsdivino/" target="_blank" rel="noopener noreferrer" className="inline-block mt-6 mr-4">
                             <img src={Linkedin} alt="LinkedIn" className="w-12 h-12 hover:opacity-75 transition-opacity" />
                         </a>
