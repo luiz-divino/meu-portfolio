2e6ed789a55bc6ac8b119b4a683567ec604330c5
unknown <nandodivinodev@gmail.com>
2025-12-12 22:30:53 -0300

    adição de animação com hover, ajustes de espaçamento e nova imagem de fundo da section

--- src/sections/ProjectsCard.js
+++ src/sections/ProjectsCard.js
@@ -1,18 +1,25 @@
 import React from "react";
 import projectsData from "../data/projects";
+import background from "../assets/img/background4.png"
 
 function ProjectsCard() {
     return (
-        <section id="projects" className="h-full flex flex-col items-center justify-center mb-8">
-            <h1 className="text-4xl font-bold py-10">Latest Projects</h1>
+        <section id="projects" className="h-full flex flex-col items-center justify-center py-8" style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover' }}>
+            <h1 className="text-4xl font-bold text-blue-600 bg-white mb-9 p-2 rounded-md"><span className="text-yellow-600">Latest </span>Projects</h1>
             <div className="flex flex-wrap justify-center gap-8 px-4 group relative w-full overflow-hidden" >
                 {projectsData.map((project) => (
-                    <div key={project.id} className="w-80 h-52 relative group overflow-hidden rounded border border-gray-900">
+                    <div key={project.id} className="w-72 h-50 relative group overflow-hidden rounded border border-gray-900">
                         <a href={project.url}
                             target="_blank"
                             rel="noopener noreferrer"
                             title={`link do projeto ${project.id}`}>
-                            <img className="w-full h-full object-cover" src={project.image} alt={`Project ${project.id}`} />
+                            <img className="w-full h-full object-cover
+          transition duration-300 ease-in-out
+          transform scale-100 shadow-md
+          hover:scale-110
+          hover:shadow-xl
+          hover:brightness-90
+                            " src={project.image} alt={`Project ${project.id}`} />
                         </a>
                     </div>
                 ))}
